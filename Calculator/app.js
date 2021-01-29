a1169dc2a9651f90dd4af4a72c7c2aa3de87ff1a
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-01-29 15:19:35 +0600

    make calculator more user friendly and added another function for exceptions

--- Calculator/app.js
+++ Calculator/app.js
@@ -5,7 +5,12 @@ const displayTop = document.querySelector('.display');
 const inputs = [];
 
 const showInnerTextToUI = function(e){
-	displayBottom.textContent = displayBottom.textContent+e.target.classList.value;
+	if(displayBottom.textContent){
+		displayBottom.textContent = '';
+		displayBottom.textContent = displayBottom.textContent+e.target.classList.value;
+	}else{
+		displayBottom.textContent = displayBottom.textContent+e.target.classList.value;
+	}
 	// return displayBottom.textContent+e.target.classList.value;
 }
 const solution = function(operator){
@@ -22,13 +27,34 @@ const solution = function(operator){
 				}else if (operator == 'div') {
 					result = `${top / bottom}`;
 				}
-				console.log(top,bottom);
+				// console.log(top,bottom);
 				result = +result;
-				console.log(typeof result ,result);
+				// console.log(typeof result ,result);
 				displayBottom.textContent = result.toFixed(2);
 				displayTop.textContent='';
 				displayMiddle.textContent='';
 			}
+const solutionExtention = function(operator,secondOperator){
+	const top = +displayTop.textContent;
+	// console.log(top);
+	const bottom = +displayBottom.textContent;
+	let result;
+	if (operator == 'add') {
+		result = `${top + bottom}`;
+	}else if (operator == 'sub') {
+		result = `${top - bottom}`;
+	}else if (operator == 'mul') {
+		result = `${top * bottom}`;
+	}else if (operator == 'div') {
+		result = `${top / bottom}`;
+	}
+	// console.log(top,bottom);
+	result = +result;
+	// console.log(typeof result ,result);
+	displayBottom.textContent = '';
+	displayTop.textContent=result.toFixed(2);
+	displayMiddle.textContent=secondOperator;
+}
 container.addEventListener('click',function(e){
 	// console.log(e.target.classList.value);
 	if(e.target.classList.value == '0' || e.target.classList.value == '1' || e.target.classList.value == '2' || e.target.classList.value == '3' ||e.target.classList.value == '4' || e.target.classList.value == '5' || e.target.classList.value == '6' || e.target.classList.value == '7' || e.target.classList.value == '8' || e.target.classList.value == '9' || e.target.classList.value == '.' ){
@@ -37,7 +63,19 @@ container.addEventListener('click',function(e){
 	}
 	if ( e.target.classList.value == '+' || e.target.classList.value == '-' || e.target.classList.value == '*' || e.target.classList.value == '/') {
 		if(displayTop.textContent){
-			console.log('top');
+			const secondOperator =e.target.classList.value;
+			// console.log(displayMiddle.textContent);
+			if (displayMiddle.textContent == '+') {
+				solutionExtention('add',secondOperator);
+			}else if (displayMiddle.textContent == '-') {
+				solutionExtention('sub',secondOperator);
+			}else if (displayMiddle.textContent == '*') {
+				solutionExtention('mul',secondOperator);
+			}else if (displayMiddle.textContent == '/') {
+				solutionExtention('div',secondOperator);
+			}
+			
+			
 
 		}else{
 			displayTop.textContent = displayBottom.textContent;

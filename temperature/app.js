4404e853c6b26f9edf262d75ed519ede20db830a
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-02-04 17:36:38 +0600

    seem ok on local server

--- temperature/app.js
+++ temperature/app.js
@@ -1,5 +1,7 @@
 const apiKey = '337ad720c924c5c104eb4f1d29b02c4d';
 const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+const city = document.getElementById('city');
+const search = document.getElementById('search_button');
 const temp = `${apiBase}?q={city name}&appid={${apiKey}}`
 let latitude;
 let longitude;
@@ -9,16 +11,37 @@ const curLoc = navigator.geolocation.getCurrentPosition(function(position){
     longitude = position.coords.longitude;
      
     my = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=337ad720c924c5c104eb4f1d29b02c4d`;
-    console.log(latitude,longitude);
+//    console.log(latitude,longitude);
     fetch(my).then(res => res.json()).then(data=>{
         document.getElementById('show_city').textContent = `${data.city.name}`;
         document.getElementById('show_temperature').textContent = (data.list[0].main.temp-273.15).toFixed(2);
         document.getElementById('weather_status').textContent = data.list[0].weather[0].main;
-        console.log(data)
+//        console.log(data)
         
-    }).catch('err')
+    }).catch('err');
     
 });
+
+search.addEventListener('click',function(e){
+    e.preventDefault();
+    const input = city.value;
+//    console.log(input);
+    
+    my = `http://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=337ad720c924c5c104eb4f1d29b02c4d`;
+//    console.log(my);
+    fetch(my).then(res=>res.json()).then(data=>{
+        if(data.cod==="404"){
+            alert('please recheck your given city name!')
+        }else{
+            document.getElementById('show_city').textContent = `${data.city.name}`;
+            document.getElementById('show_temperature').textContent = (data.list[0].main.temp-273.15).toFixed(2);
+            document.getElementById('weather_status').textContent = data.list[0].weather[0].main;
+        }
+//        console.log(data);
+    }).catch('error');
+    
+    
+})
 
 
 
